ce4e73ad210504b6a2893f83b36cfe748915b782
Larissa <larissamroehrig@gmail.com>
2022-02-13 09:00:25 +0100

    added comments

--- matching/l1_pattern.py
+++ matching/l1_pattern.py
@@ -2,17 +2,40 @@ l1_resection_pattern = [
     {
         "RIGHT_ID": "anchor_resection",
         "RIGHT_ATTRS": {
-        "ORTH": {"IN": ["resection", "resected", "removed", "excised", "cut out"]}
+            "ORTH": {"IN": ["resection", "resected", "removed", "excised", "cut out"]}
         },
     },
     {
         "LEFT_ID": "anchor_resection",
-        "REL_OP": ";*",
+        "REL_OP": ">>",
         "RIGHT_ID": "resection_subject",
         "RIGHT_ATTRS": {"DEP": "nsubjpass", "_": {"has_ileal": True}},
     },
-]
-
+    {
+        "LEFT_ID": "anchor_resection",
+        "REL_OP": ">>",
+        "RIGHT_ID": "resection_obj",
+        "RIGHT_ATTRS": {"DEP": "amod"},
+    }
+    
+l1_pattern_v2 = [
+    {
+        "RIGHT_ID": "anchor_resection",
+        "RIGHT_ATTRS": {
+            "ORTH": {"IN": [{"has_ileal": True}]}},
+    },
+    {
+        "LEFT_ID": "anchor_resection",
+        "REL_OP": ">>",
+        "RIGHT_ID": "resection_subject",
+        "RIGHT_ATTRS": {"DEP": "nsubj"},
+    },
+    {
+        "LEFT_ID": "anchor_resection",
+        "REL_OP": ">",
+        "RIGHT_ID": "resection_object",
+        "RIGHT_ATTRS": {"DEP": {"IN": ["amod", "compound"]}},
+    },
 
 l1_endoscopy_patterns = [
     {
@@ -21,7 +44,7 @@ l1_endoscopy_patterns = [
     },
     {
         "LEFT_ID": "anchor_endoscopy",
-        "REL_OP": ">",
+        "REL_OP": ">>",
         "RIGHT_ID": "endoscopy_subject",
         "RIGHT_ATTRS": {"DEP": "nmod", "_": {"has_cecum": True}},
     },
